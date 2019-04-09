c36d2a0ee3e3c57bde46aa7db196b78341647338
sunsokleng <sokleng1983@gmail.com>
2019-04-10 01:00:00 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,40 +6,46 @@
     <input type="text" v-model="lastname"><br/>
     <button @click="handleSave()">Save</button>
    
-   <ul v-bind:key= 'list' v-for = "(list, index) in lists">
+   <ul :key= 'index' v-for = "(list, index) in lists">
      
      {{list.firstname}} {{list.lastname}}
      <button @click="handleDelete(index)">Delete</button>
+     <button @click="handleUpdate(index)">Update</button>
    </ul>
   </div>
 </template>
 
 <script>
-
 export default {  
   
   data() {
-    
     return {
-      lists:[
-      {
-        firstname:'',
-        lastname: ''
-      }
-      ]
+      firstname: "",
+      lastname: "",
+      lists:[]
     }
   },
   
  methods:{
+   
    handleSave(){
-     this.lists.push({firstname:this.firstname, lastname:this.lastname});
+     const {firstname, lastname}=this;
+     this.lists.push({firstname, lastname})
+     //this.lists.push({firstname:this.firstname, lastname:this.lastname});
      this.handleClear();
    },
 
     handleDelete(index){
       this.lists.splice(index,1);
     },
-   handleClear(){
+    
+    handleUpdate(index){
+      this.firstname=this.lists[index].firstname;
+      this.lastname=this.lists[index].lastname;
+      this.lists.splice(index,1);
+    },
+    
+    handleClear(){
      this.firstname = "";
      this.lastname = "";
    }
